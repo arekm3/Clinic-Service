7a1045505f011ca2426eb7c32249df27ddeb65fa
Arek <arekmontsko@wp.pl>
2021-03-20 12:14:54 +0100

    first commit

---
+++ src/main/java/com/ClinicService/ClinicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ClinicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ClinicService/ClinicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClinicService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
