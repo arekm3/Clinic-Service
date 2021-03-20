bece0f3f6ceb931f2a58a4ec6f2c6bb430054f6d
Kamil <kamil.litwin230@wp.pl>
2021-03-20 13:41:10 +0100

    Added repo, services and config

---
+++ src/main/java/com/ClinicService/configuration/WebSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.ClinicService.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
