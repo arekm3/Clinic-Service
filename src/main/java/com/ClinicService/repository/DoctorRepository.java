66c40b28a071e96521127f08f8bde4d7b9be4d95
Kamil <kamil.litwin230@wp.pl>
2021-03-21 09:22:34 +0100

    fixed repo

--- src/main/java/com/ClinicService/repository/DoctorRepository.java
+++ src/main/java/com/ClinicService/repository/DoctorRepository.java
@@ -1,9 +1,11 @@
 package com.ClinicService.repository;
 
 import com.ClinicService.model.Doctor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DoctorRepository {
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
     Doctor findById(int id);
 }
--- src/main/java/com/ClinicService/repository/VisitRepository.java
+++ src/main/java/com/ClinicService/repository/VisitRepository.java
@@ -1,7 +1,9 @@
 package com.ClinicService.repository;
 
-import com.ClinicService.model.Visit;
 
-public interface VisitRepository {
+import com.ClinicService.model.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<Visit, Integer> {
     Visit getById(int id);
 }
