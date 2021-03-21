100b5c1646c7d55dd2ba280d8d45f311525aa0a4
Kamil <kamil.litwin230@wp.pl>
2021-03-21 09:23:07 +0100

    fixed repo

--- src/main/java/com/ClinicService/repository/VisitRepository.java
+++ src/main/java/com/ClinicService/repository/VisitRepository.java
@@ -3,7 +3,9 @@ package com.ClinicService.repository;
 
 import com.ClinicService.model.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VisitRepository extends JpaRepository<Visit, Integer> {
     Visit getById(int id);
 }
